e85a65233e64037d64a6e2336cf1f293767d6aaa
Virgil <virgilavram.dev@gmail.com>
2022-05-31 12:35:59 +0200

    Render the map using vue-leaflet lib

---
+++ src/components/Map/MapContainer.vue
@@ -0,0 +1,38 @@
+<template>
+  <l-map style="height: 650px " :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution">
+    </l-tile-layer>
+    <l-marker :lat-lng="markerLatLng">
+      <l-popup>Live ?</l-popup>
+    </l-marker>
+  </l-map>
+</template>
+
+<script lang="ts">
+import 'leaflet/dist/leaflet.css'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { LMap, LTileLayer, LMarker, LPopup } from '@vue-leaflet/vue-leaflet'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MapContainer',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 16,
+      center: [52.080678, 5.236457],
+      markerLatLng: [52.080678, 5.236457]
+    }
+  }
+})
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="home">
+    <MapContainer />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import MapContainer from '@/components/Map/MapContainer.vue'
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  components: {
+    MapContainer
+  }
 })
 </script>
