bf6312d17cef7d1bff424acd74b35fb61a71a5c2
Virgil <virgilavram.dev@gmail.com>
2022-06-03 13:11:29 +0200

    Toggle choices container on user input

--- src/components/Map/FilterMushrooms.vue
+++ src/components/Map/FilterMushrooms.vue
@@ -1,9 +1,9 @@
 <template id="">
-  <div class="">
+  <div class=""  @click="toggleChoices">
     <span>filter</span>
     <span>by {{ type }}</span>
   </div>
-  <ul>
+  <ul v-if="showChoices">
     <li
       v-for="(choice, i) in choices"  :key="i"
       @click="sort(choice.name, type)"
@@ -15,15 +15,18 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Color, Mushroom } from '@/api/front-end api.ts'
 
 export default defineComponent({
   name: 'FilterMushrooms',
   props: ['choices', 'type', 'sort', 'mushrooms'],
   data () {
     return {
-      showChoices: false,
-      color: Color
+      showChoices: false
+    }
+  },
+  methods: {
+    toggleChoices () {
+      !this.showChoices ? this.showChoices = true : this.showChoices = false
     }
   }
 })
