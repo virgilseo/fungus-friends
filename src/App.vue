19f318662bea46e19ff862ffde7c62b58d385c4c
Virgil <virgilavram.dev@gmail.com>
2022-06-09 12:46:37 +0200

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
+  <Header />
   <router-view/>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Header from '@/components/Globals/Header.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Header
+  }
+})
+</script>
 <style>
 @import "./assets/stylesheets/app.css";
 </style>
---
+++ src/components/Globals/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header-container">
+    <header class="header">
+      <h1 class="header-title">Fungus Friends</h1>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Header'
+})
+</script>
