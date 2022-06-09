58d537a8fc6a679f10bd72f5d9d2d78483bb0a48
Virgil <virgilavram.dev@gmail.com>
2022-06-09 12:50:18 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <Header />
   <router-view/>
+  <Footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Header from '@/components/Globals/Header.vue'
+import Footer from '@/components/Globals/Footer.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    Header
+    Header,
+    Footer
   }
 })
 </script>
---
+++ src/components/Globals/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer-container">
+    <footer>
+      <p>© 2022 Virgil Avram</p>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Footer'
+})
+</script>
